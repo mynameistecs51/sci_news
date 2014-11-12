748a71db86e23e7f1b2c745107212e26bac1bd1b
chaiwat <mynameistecs51.gmail.com>
2014-11-12 22:25:28 +0700

    chaiwat update file upload

--- application/views/list_news.php
+++ application/views/list_news.php
@@ -9,22 +9,29 @@
 	<?php 
 	foreach ($show_news as $detail => $row) 	{		// show detail news 		
 
-		echo '<div class=" col-sx-8 col-sm-5 " style="word-wrap:break-word; display:block; ">';
+		echo '<div class=" col-sx-8 col-sm-10" style="word-wrap:break-word; display:block; ">';
 		echo '<div class="panel panel-info">';
 		echo '<div class="panel panel-heading"> <h4>'.$row->news_title.'</h></div>';
-		echo '<div class="panel panel-body"><p>';
-						//show detail text
-		echo $row->news_detail;
+		echo '<div class="panel panel-body"><p>';						
+		echo $row->news_detail;		//show detail text
 		echo '</p></div>';
 		echo "</div>";	//end div=class panel
 
-		echo '<div class="col-md-4">';  //----------show picture news-------------//
+		
 		$picture_name_array = explode(',', $row->news_file_upload);
 		foreach ($picture_name_array as $key => $value_detail) { //show picture 
+			
+			echo '<div class="row">';//----------show picture news-------------//
+			echo ' <div class="col-xs-6 col-md-3">';
+			echo '  <a href="#" class="thumbnail">';
 			echo '<img src='.base_url().'file_upload/pict_news/'.$value_detail.' alt="" width=350px height=220px> &nbsp;';
+			echo '</a>';
+			echo '</div>';
+			echo '</div>';
 		}	
-		echo "</div>";  //-- end div class col-md-4 ----//
+		
 	}
+	
 	?>
 </div>
 <?php $this->load->view('footer');?>
