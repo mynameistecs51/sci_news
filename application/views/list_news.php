79c928d16b4f6b3ece398f78d3355adacdebe093
chaiwat <mynameistecs51@gmail.com>
2014-11-13 11:01:25 +0700

    chaiwat delete picture

--- application/views/list_news.php
+++ application/views/list_news.php
@@ -8,30 +8,26 @@
 
 	<?php 
 	foreach ($show_news as $detail => $row) 	{		// show detail news 		
+		?>
+		<div class=" col-sx-8 col-sm-10" style="word-wrap:break-word; display:block; ">
+			<div class="panel panel-info">
+				<div class="panel panel-heading"> <h4><?php echo $row->news_title;?></h4></div>
+				<div class="panel panel-body"><p>					
+					<?php echo $row->news_detail;	?>	<!-- //show detail text -->
+				</p></div>
+			</div>	<!-- //end div=class panel -->
+			<?php 			
 
-		echo '<div class=" col-sx-8 col-sm-10" style="word-wrap:break-word; display:block; ">';
-		echo '<div class="panel panel-info">';
-		echo '<div class="panel panel-heading"> <h4>'.$row->news_title.'</h></div>';
-		echo '<div class="panel panel-body"><p>';						
-		echo $row->news_detail;		//show detail text
-		echo '</p></div>';
-		echo "</div>";	//end div=class panel
-
-		
-		$picture_name_array = explode(',', $row->news_file_upload);
-		foreach ($picture_name_array as $key => $value_detail) { //show picture 
-			
-			echo '<div class="row">';//----------show picture news-------------//
-			echo ' <div class="col-xs-6 col-md-3">';
-			echo '  <a href="#" class="thumbnail">';
-			echo '<img src='.base_url().'file_upload/pict_news/'.$value_detail.' alt="" width=350px height=220px> &nbsp;';
-			echo '</a>';
-			echo '</div>';
-			echo '</div>';
+			$picture_name_array = explode(',', $row->news_file_upload);
+			foreach ($picture_name_array as $key => $value_detail) { //show picture 
+				echo '<div class="col-xs-6 col-md-3">';
+				echo '<div class="thumbnail">';
+				echo '<img src='.base_url().'file_upload/pict_news/'.$value_detail.' alt="" width=350px height=220px> &nbsp;';
+				echo '</div>';	//  <!--  ./ end class="thumbnail"-->
+				echo '</div>';	//<!-- ./ end class = "col-xs-6 col-md-3" -->
+			}
 		}	
-		
-	}
-	
-	?>
+		?>
+	</div>
 </div>
 <?php $this->load->view('footer');?>
