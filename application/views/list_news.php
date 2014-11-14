32062fe745f9553b4598f1acac3ffdc1ae08f15d
chaiwat <mynameistecs51.gmail.com>
2014-11-14 21:34:18 +0700

    chaiwat update js

--- application/views/list_news.php
+++ application/views/list_news.php
@@ -1,9 +1,25 @@
 <?php $this->load->view("header");?>
+<!--  jquery lightbox -->
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
+<script src="<?php echo base_url().'js/jquery.mousewheel.pack.js';?>"></script>
+<script src="<?php echo base_url().'js/jquery-1.10.2.min.js';?>"></script>
+<!-- css lightbox -->
+<link href="<?php echo base_url().'css/plugins/lightbox.css';?>" rel="stylesheet">
+
+<!-- ./end  -->
 <div class="row">
 	<div class="col-lg-12">
 		<h1 class="page-header">รายการข่าว</h1>
 	</div>    <!-- /.col-lg-12 -->
 </div>	<!-- /.row -->
+<script type="text/javascript">
+	$(document).ready(function() {
+		$(".fancybox").fancybox({
+			openEffect	: 'none',
+			closeEffect	: 'none'
+		});
+	});
+</script>
 <div class="row" >
 
 	<?php 
@@ -24,7 +40,9 @@
 			foreach ($picture_name_array as $key => $value_detail) { //show picture 
 				echo '<div class="col-xs-6 col-md-3">';
 				echo '<div class="thumbnail">';
+				echo '<a class="fancybox" rel="gallery1" href='.base_url().'file_upload/pict_news/'.$value_detail.'  title="Westfield Waterfalls - Middletown CT Lower (Graham_CS)">';
 				echo '<img src='.base_url().'file_upload/pict_news/'.$value_detail.' alt="" width=350px height=220px> &nbsp;';
+				echo '</a>';
 				echo '</div>';	//  <!--  ./ end class="thumbnail"-->
 				echo '</div>';	//<!-- ./ end class = "col-xs-6 col-md-3" -->
 			}
