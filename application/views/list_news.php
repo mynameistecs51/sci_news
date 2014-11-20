b4408e966c0f1dfd2aeee7badcaa2a600eb42f7e
chaiwat <mynameistecs51@gmail.com>
2014-11-20 10:24:12 +0700

    chaiwat update show list news

--- application/views/list_news.php
+++ application/views/list_news.php
@@ -1,50 +1,50 @@
 <?php $this->load->view("header");?>
 <!-- Add jQuery library -->
-<script type="text/javascript" src="<?php echo base_url().'js/jquery-1.10.2.min.js';?>"></script>
+        <script type="text/javascript" src="<?php echo base_url().'js/jquery-1.10.2.min.js';?>"></script>
 
-<!-- Add mousewheel plugin (this is optional) -->
-<script type="text/javascript" src="<?php echo base_url().'js/jquery.mousewheel.pack.js?v=3.1.3';?>"></script>
+        <!-- Add mousewheel plugin (this is optional) -->
+        <script type="text/javascript" src="<?php echo base_url().'js/jquery.mousewheel.pack.js?v=3.1.3';?>"></script>
 
-<!-- Add fancyBox main JS and CSS files -->
-<script type="text/javascript" src="<?php echo base_url().'source/jquery.fancybox.pack.js?v=2.1.5';?>"></script>
-<link rel="stylesheet" type="text/css" href="<?php echo base_url().'source/jquery.fancybox.css?v=2.1.5';?>" media="screen" />
+        <!-- Add fancyBox main JS and CSS files -->
+        <script type="text/javascript" src="<?php echo base_url().'source/jquery.fancybox.pack.js?v=2.1.5';?>"></script>
+        <link rel="stylesheet" type="text/css" href="<?php echo base_url().'source/jquery.fancybox.css?v=2.1.5';?>" media="screen" />
 
-<script type="text/javascript">
-	$(document).ready(function() {
-			/*
-			 *  Simple image gallery. Uses default settings
-			 */
+        <script type="text/javascript">
+            $(document).ready(function() {
+            /*
+             *  Simple image gallery. Uses default settings
+             */
 
-			 $('.fancybox').fancybox();
+             $('.fancybox').fancybox();
 
-			/*
-			 *  Different effects
-			 */
+            /*
+             *  Different effects
+             */
 
-			// Change title type, overlay closing speed
-			$(".fancybox-effects-a").fancybox({
-				helpers: {
-					title : {
-						type : 'outside'
-					},
-					overlay : {
-						speedOut : 0
-					}
-				}
-			});
+            // Change title type, overlay closing speed
+            $(".fancybox-effects-a").fancybox({
+                helpers: {
+                    title : {
+                        type : 'outside'
+                    },
+                    overlay : {
+                        speedOut : 0
+                    }
+                }
+            });
 
-		});
-</script>
-<style type="text/css">
-	.fancybox-custom .fancybox-skin {
-		box-shadow: 0 0 50px #222;
-	}
+        });
+        </script>
+        <style type="text/css">
+            .fancybox-custom .fancybox-skin {
+                box-shadow: 0 0 50px #222;
+            }
 
-	tbody {
-		max-width: 700px;
-		margin: 0 auto;
-	}
-</style>
+            tbody {
+                max-width: 700px;
+                margin: 0 auto;
+            }
+        </style>
 <!-- ./end show slide picture  lightbox-->
 <div class="row" >
 
@@ -67,7 +67,7 @@
 							foreach ($picture_name_array as $key => $value_detail) { //show picture 
 								?>
 								<a class="fancybox" href="<?php echo base_url().'file_upload/pict_news/'.$value_detail;?>" data-fancybox-group="gallery" title="<?php echo $row->news_title;?>">
-									<img src="<?php echo base_url().'file_upload/pict_news/'.$value_detail;?>" alt="" style="width:128px;"/>
+									<img src="<?php echo base_url().'file_upload/pict_news/'.$value_detail;?>" alt=""/>
 								</a>
 								<?php
 							}
