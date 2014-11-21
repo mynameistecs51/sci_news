1cf319c885cc7daa0abd6168f2342ec15b53545d
--help <mynameistecs51@gmail.com>
2014-11-22 00:38:15 +0700

    chaiwat update show detail news & picture

--- application/views/footer.php
+++ application/views/footer.php
@@ -5,7 +5,7 @@
        <!-- <script src="<?php echo base_url().'js/jquery.js';?>"></script> -->
 
         <!-- Bootstrap Core JavaScript -->
-        <script src="<?php echo base_url().'js/bootstrap.min.js';?>"></script>
+       <!-- <script src="<?php echo base_url().'js/bootstrap.min.js';?>"></script> -->
 
         <!-- Metis Menu Plugin JavaScript -->
         <script src="<?php echo base_url().'js/plugins/metisMenu/metisMenu.min.js';?>"></script>
--- application/views/picture.php
+++ application/views/picture.php
@@ -1,7 +1,7 @@
 <?php $this->load->view('header');?>
 
-	<script type="text/javascript">
-		$(document).ready(function() {
+<script type="text/javascript">
+	$(document).ready(function() {
 			/*
 			 *  Simple image gallery. Uses default settings
 			 */
@@ -25,27 +25,37 @@
 			});
 
 		});
-	</script>
-	<style type="text/css">
-		.fancybox-custom .fancybox-skin {
-			box-shadow: 0 0 50px #222;
-		}
+</script>
+<style type="text/css">
+	.fancybox-custom .fancybox-skin {
+		box-shadow: 0 0 50px #222;
+	}
 
-		tbody {
-			max-width: 700px;
-			margin: 0 auto;
-		}
-	</style>
+	tbody {
+		max-width: 700px;
+		margin: 0 auto;
+	}
+</style>
 
+<div class="row">
+	<div class="col-lg-12">
+		<h1 class="page-header">รายละเอียดของข่าว</h1>
+	</div>
+	<!-- /.col-lg-12 -->
+</div>
 
 <tbody>
 	<p>
 		<?php 
 		foreach ($get_news_id as $detail => $row) 	{
-			
+			echo '<div class="panel panel-primary">';
+				echo '<div class="panel-heading">'.$row->news_title.'</div>';
+					echo '<div class="panel-body">';
+						echo $row->news_detail;
+					echo '</div>';
+				echo '</div>';
 			$picture_name_array = explode(',', $row->news_file_upload);
-			foreach ($picture_name_array as $key => $value_detail) { //show picture 
-			
+			foreach ($picture_name_array as $key => $value_detail) { //show picture 			
 				?>
 				<a class="fancybox" href="<?php echo base_url().'file_upload/pict_news/'.$value_detail;?>" data-fancybox-group="gallery" title="<?php echo $row->news_title;?>">
 					<img src="<?php echo base_url().'file_upload/pict_news/'.$value_detail;?>" alt="" style="width:128px;"/>
