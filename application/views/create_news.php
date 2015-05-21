ee7802605e0183abde3b43b62fd4cb6864bdb4f3
chaiwat <mynameistecs51@gmail.com>
2015-05-21 16:45:08 +0700

    chaiwat create box drag&move

--- application/views/create_news.php
+++ application/views/create_news.php
@@ -6,7 +6,6 @@
   <link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
   <script src="//code.jquery.com/jquery-1.10.2.js"></script>
   <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
-  <script type="text/javascript" src="//code.jquery.com/jquery-latest.js"></script>
   <link rel="stylesheet" href="/resources/demos/style.css">
   <style>
     .draggable { width: 90px; height: 90px; padding: 0.5em; float: left; margin: 0 10px 10px 0; }
@@ -14,13 +13,35 @@
     #draggable { cursor: n-resize; }
     /*#draggable2 { cursor: e-resize; }*/
     #containment-wrapper { width: 95%; height:500px; border:2px solid #ccc; padding: 10px; }
+
     h3 { clear: left; }
   </style>
   <script>
     $(function() {
+      jQuery(function() {
+        jQuery(".dragImg").draggable({
+        //  use a helper-clone that is append to 'body' so is not 'contained' by a pane
+        helper: function() {
+          return jQuery(this).clone().appendTo(' body').css({
+            'zIndex': 5
+          });
+        },
+        cursor: 'move',
+        containment: "document"
+      });
 
-      $( "#draggable1" ).draggable({ containment: "#containment-wrapper", scroll: false });
-      $( "#draggable2" ).draggable({ containment: "#containment-wrapper", scroll: false });
+        jQuery('.ui-layout-center').droppable({
+          activeClass: 'ui-state-hover',
+          accept: '.dragImg',
+          drop: function(event, ui) {
+            if (!ui.draggable.hasClass("dropped"))
+              jQuery(this).append(jQuery(ui.draggable).clone().addClass("dropped").draggable());
+          }
+        });
+      });
+
+      $(".dragImg").resizable();
+
     });
   </script>
 </head>
@@ -28,16 +49,13 @@
 
   <h3>สร้าง content ขนาดกระดาษ A3 แนวนอน:</h3>กลับหน้าหลัก <?php echo anchor('sci_con/','index');?>
   <a class="add-box" href="#">add textarea  </a>
-  <div id="containment-wrapper">
-    <div id="draggable1" class="draggable ui-widget-content">
-      <p>I'm contained within the box</p>
-    </div>
-
-    <div id="draggable2" class="draggable ui-widget-content">
-      <p>I'm contained within the box</p>
-    </div>
+  
+  <div class="dragImg"><img src="http://placehold.it/80x80">
   </div>
-<!-- -------text add textarea -->
+
+  <div class="ui-layout-center" id="containment-wrapper"> 
+
+  </div>
 
 </body>
 </html>
