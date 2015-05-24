28cd8ed73fc205f65b15f7385c090f47ee43b623
chaiwat <mynameistecs51@gmail.com>
2015-05-24 15:30:42 +0700

    chaiwat update create_news page

--- application/views/create_news.php
+++ application/views/create_news.php
@@ -6,7 +6,7 @@
   <link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
   <script src="//code.jquery.com/jquery-1.10.2.js"></script>
   <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
-  <link rel="stylesheet" href="/resources/demos/style.css">
+ <!--  <link rel="stylesheet" href="/resources/demos/style.css"> -->
   <style>
     /*#draggable { width: 90px; height: 90px; padding: 0.5em; float: left; margin: 0 10px 10px 0; }*/
     /*#draggable, #draggable2 { margin-bottom:20px; }*/
