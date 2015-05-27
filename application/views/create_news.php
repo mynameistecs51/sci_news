85df219b5aa2318b04c881ea469b90b5290f419e
chaiwat <mynameistecs51@gmail.com>
2015-05-27 14:37:43 +0700

    chaiwat update function add picture in box

--- application/views/create_news.php
+++ application/views/create_news.php
@@ -1,78 +1,100 @@
 <!doctype html>
 <html lang="en">
 <head>
-  <meta charset="utf-8">
-  <title>Create news </title>
-  <link rel="stylesheet" href="<?php echo base_url();?>js/jquery-ui-1.11.4.custom/jquery-ui.css">
+	<meta charset="utf-8">
+	<title>Create news </title>
+	<link rel="stylesheet" href="<?php echo base_url();?>js/jquery-ui-1.11.4.custom/jquery-ui.css">
 
-  <!--  <link rel="stylesheet" href="/resources/demos/style.css"> -->
-  <style>
-    #draggable { width: 50px; height: 70px; padding: 0.5em; }
-    #draggable { cursor: move; }
-    #containment-wrapper { width: 95%; height:500px; border:2px solid #ccc; padding: 10px; }
-    .resizable { width: 150px; height: 150px; padding: 0.5em; }
-    /*.title{blackground:lightblue;height: 50px;cursor: pinter;margin-bottom: 20px;}*/
-    .delete{color:red; display: none;}
-    #draggable:hover .delete{display: block}
-    h3 { clear: left; }
-  </style>
+	<!--  <link rel="stylesheet" href="/resources/demos/style.css"> -->
+	<style>
+		#draggable {width:130px; height:130px; padding: 0.5em; }
+		#draggable { cursor: move; }
+		#containment-wrapper { width: 95%; height:500px; border:2px solid #ccc; padding: 10px; }
+		.resizable { width: 150px; height: 150px; padding: 0.5em; }
+		/*.title{blackground:lightblue;height: 50px;cursor: pinter;margin-bottom: 20px;}*/
+		.delete{color:red; display: none;}
+		.hiddent-file{display: none;}
+		#draggable:hover .delete{display: block}
+		h3 { clear: left; }
+	</style>
 
 </head>
 <body>
 
-  <h3>สร้าง content ขนาดกระดาษ A3 แนวนอน:</h3>กลับหน้าหลัก <a href="http://sci.udru.ac.th/scinews/sci_con">index</a>  <a class="add-box" href="#">add textarea  </a>
-  
-  <!-- <div class="draggable" id="resize"></div> -->
-  <div id="draggable" class="ui-widget-content">
-    <p> picture </p>
-    <a class="delete" href="#">delete </a>
-  </div>
-  <div >
-    <p>add text box</p>
-  </div>
-  <div class="ui-layout-center" id="containment-wrapper"> 
+	<h3>สร้าง content ขนาดกระดาษ A3 แนวนอน:</h3>กลับหน้าหลัก <a href="index">index</a>  <a class="add-box" href="#">add textarea  </a>
 
-  </div>
-  <!-- jquery  -->
-  <script src="<?php echo base_url();?>js/jquery-ui-1.11.4.custom/external/jquery/jquery.js"></script>
-  <script src="<?php echo base_url();?>js/jquery-ui-1.11.4.custom/jquery-ui.min.js"></script>    
-  <script>
+	<!-- <div class="draggable" id="resize"></div> -->
 
-    // function fnResize(){
-    //   $(".resizable").resizable();
-    // }
+	<div id="draggable" class="ui-widget-content" name="draggable[]">
+		<img id="show_pic[]" name="show_pic[]" src="<?php echo base_url().'image/pict_admin/no-image.jpg';?>" alt="" style="width:130px; height:130px" />
 
-    $(function() {
-      $("#draggable").draggable({
-        helper: function() {
-          return jQuery(this).clone().appendTo(' body').css({
-            'zIndex': 5
-          });
-        },
-        cursor: 'move',
-        containment: "document"
-      });
+		<input type="file" id="images[]" class="hiddent-file" name="images[]" size="20" onchange="PreviewImage();" />
+		<a class="delete" href="#">delete </a>
 
-      $('.ui-layout-center').droppable({
-        activeClass: 'ui-state-hover',
-        accept: '#draggable',
-        tolerance:'pointer',
+	</div>
 
-        drop: function(event, ui) {
-          if (!ui.draggable.hasClass("dropped")){
-            $(this).append($(ui.draggable).clone().addClass("dropped").draggable().resizable());
-          }
+	<br/><br/>
+	<!-- <input type="file" id="images[]" class="form-control" name="images[]" size="20" onchange="PreviewImage();" multiple=""/> -->
 
-          $('.delete').click(function(){
-          $(this).parent().remove();
-          return false;
-          });
-        }
+	<div >
+		<p>add text box</p>
+	</div>
 
-      });
+	<div class="ui-layout-center" id="containment-wrapper"> 
+		<!-- display box latter news -->
+	</div>
+	<!-- jquery  -->
+	<script src="<?php echo base_url();?>js/jquery-ui-1.11.4.custom/external/jquery/jquery.js"></script>
+	<script src="<?php echo base_url();?>js/jquery-ui-1.11.4.custom/jquery-ui.min.js"></script>    
+	<script>
 
+		$(function() {
 
-    });
-  </script>
+			$("#draggable").draggable({
+				helper: function() {
+					return jQuery(this).clone().appendTo(' body').css({
+						'zIndex': 5
+					});
+				},
+				cursor: 'move',
+				containment: "document"
+			});
+
+			$('.ui-layout-center').droppable({
+				activeClass: 'ui-state-hover',
+				accept: '#draggable',
+				tolerance:'pointer',
+
+				drop: function(event, ui) {
+					if (!ui.draggable.hasClass("dropped")){
+						$(this).append($(ui.draggable).clone().addClass("dropped").draggable().resizable().dblclick( function(){
+							$('input[type="file"]').click(); 
+						}));
+
+						$('.delete').click(function(){
+							$(this).parent().remove();
+							return false;
+						});
+						function PreviewImage() {
+
+							var oFReader = new FileReader();
+
+							oFReader.readAsDataURL(document.getElementById("images[]").files[0]);
+
+							oFReader.onload = function (oFREvent) {
+
+								document.getElementById("show_pic[]").src = oFREvent.target.result;
+
+							};
+
+					} //end function previewimage
+
+				}
+
+			}
+		});
+
+		});
+</script>
 </body>
 </html>
