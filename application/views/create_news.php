bf79f52952a9d254b1d41236ddc9a37c6f1d26ef
chaiwat <mynameistecs51@gmail.com>
2015-05-27 10:21:11 +0700

    chaiat update function delete this box

--- application/views/create_news.php
+++ application/views/create_news.php
@@ -7,10 +7,13 @@
 
   <!--  <link rel="stylesheet" href="/resources/demos/style.css"> -->
   <style>
-    #draggable { width: 50px; height: 50px; padding: 0.5em; }
+    #draggable { width: 50px; height: 70px; padding: 0.5em; }
     #draggable { cursor: move; }
     #containment-wrapper { width: 95%; height:500px; border:2px solid #ccc; padding: 10px; }
     .resizable { width: 150px; height: 150px; padding: 0.5em; }
+    /*.title{blackground:lightblue;height: 50px;cursor: pinter;margin-bottom: 20px;}*/
+    .delete{color:red; display: none;}
+    #draggable:hover .delete{display: block}
     h3 { clear: left; }
   </style>
 
@@ -22,6 +25,7 @@
   <!-- <div class="draggable" id="resize"></div> -->
   <div id="draggable" class="ui-widget-content">
     <p> picture </p>
+    <a class="delete" href="#">delete </a>
   </div>
   <div >
     <p>add text box</p>
@@ -29,13 +33,15 @@
   <div class="ui-layout-center" id="containment-wrapper"> 
 
   </div>
+  <!-- jquery  -->
   <script src="<?php echo base_url();?>js/jquery-ui-1.11.4.custom/external/jquery/jquery.js"></script>
   <script src="<?php echo base_url();?>js/jquery-ui-1.11.4.custom/jquery-ui.min.js"></script>    
   <script>
 
-    function fnResize(){
-      $(".resizable").resizable();
-    }
+    // function fnResize(){
+    //   $(".resizable").resizable();
+    // }
+
     $(function() {
       $("#draggable").draggable({
         helper: function() {
@@ -53,10 +59,19 @@
         tolerance:'pointer',
 
         drop: function(event, ui) {
-          if (!ui.draggable.hasClass("dropped"))
+          if (!ui.draggable.hasClass("dropped")){
             $(this).append($(ui.draggable).clone().addClass("dropped").draggable().resizable());
+          }
+
+          $('.delete').click(function(){
+          $(this).parent().remove();
+          return false;
+          });
         }
+
       });
+
+
     });
   </script>
 </body>
